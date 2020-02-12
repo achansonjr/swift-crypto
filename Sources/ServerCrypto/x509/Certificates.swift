e5f7b98ff0c55b39265ccd9c559b4ad4aa65e628
Ryan Lovelett <RYAN.A.LOVELETT@saic.com>
2020-02-12 17:40:38 -0600

    Integrate Certificates with BIOIterator

--- Sources/ServerCrypto/x509/Certificates.swift
+++ Sources/ServerCrypto/x509/Certificates.swift
@@ -75,44 +75,8 @@ extension Certificates {
   /// - Parameter bytes: The PEM buffer to read certificates from.
   /// - Throws: If an error is encountered while reading certificates.
   init<B: ContiguousBytes>(PEMBytes: B) throws {
-      CCryptoBoringSSL_ERR_clear_error()
-      defer {
-          CCryptoBoringSSL_ERR_clear_error()
-      }
-
-      self = try PEMBytes.withUnsafeBytes { (ptr) -> Certificates in
-          let bio = CCryptoBoringSSL_BIO_new_mem_buf(ptr.baseAddress, CInt(ptr.count))!
-          defer {
-              CCryptoBoringSSL_BIO_free(bio)
-          }
-          return try createCertificates(fromBIO: bio)
-      }
+      let iterator = try BIOIterator(PEMBytes: PEMBytes)
+      self.contents = [Certificate](AnySequence { iterator })
   }
 
-  /// Reads `Certificate`s from the given BIO.
-  internal func createCertificates(fromBIO: UnsafeMutablePointer<BIO>) throws -> Certificates {
-      guard let x509 = CCryptoBoringSSL_PEM_read_bio_X509_AUX(fromBIO, nil, nil, nil) else {
-          throw CryptoCertificateError.failedToLoadCertificate
-      }
-
-      // var certificates = [Certificate(withOwnedReference: x509)]
-      var certificates: Certificates = Certificates()
-      certificates.add(Certificate(withOwnedReference: x509))
-    
-      while let x = CCryptoBoringSSL_PEM_read_bio_X509(fromBIO, nil, nil, nil) {
-          certificates.add(Certificate(withOwnedReference: x))
-      }
-
-      let err = CCryptoBoringSSL_ERR_peek_error()
-
-      // If we hit the end of the file then it's not a real error, we just read as much as we could.
-      if CCryptoBoringSSLShims_ERR_GET_LIB(err) == ERR_LIB_PEM && CCryptoBoringSSLShims_ERR_GET_REASON(err) == PEM_R_NO_START_LINE {
-          CCryptoBoringSSL_ERR_clear_error()
-      } else {
-          throw CryptoCertificateError.failedToLoadCertificate
-      }
-
-      // self = certificates
-      return certificates
-  }
 }
